a57cc5b760e90f6c798d5099e9c2267b6741fb40
Srishti Sharma <srishtishar@gmail.com>
2022-01-05 10:07:17 +0530

    updated files

--- src/utils.js
+++ src/utils.js
@@ -5,7 +5,6 @@ const sortTodoList = (todolist) => {
 
 const addToList = (taskObj) => {
     console.log(taskObj);
-    //let taskArray = JSON.parse(localStorage.getItem('todoList'));
     let taskArray = getTodoList();
     taskArray.push(taskObj);
     let sortedTaskArray = sortTodoList(taskArray);
@@ -13,13 +12,11 @@ const addToList = (taskObj) => {
     localStorage.setItem('todoList', newTaskArray);
     console.log(newTaskArray);
     console.log(JSON.parse(localStorage.getItem('todoList')));
-    //console.log(sortTodoList(JSON.parse(localStorage.getItem('todoList'))));
+    
 }
 
 const deleteFromList = (taskId) => {
-    /// find object where id matches taskId and delete, then sort the list
     console.log(taskId);
-    //let taskArray = JSON.parse(localStorage.getItem('todoList'));
     let taskArray = getTodoList();
     taskArray = taskArray.filter( task => task.id !== taskId);
     console.log(taskArray);
@@ -37,16 +34,12 @@ const getTodoList = () => {
 } 
 
 const markAsDone = (taskId) => {
-    /// find object where id matchces taskid and update status, put priority to 0,
-    // then sort the list
     console.log(taskId);
     let taskArray = getTodoList();
     let taskObj = taskArray.find(task => task.id === taskId);
     taskObj.status = 'done';
     taskObj.priorityVal = 0;
     console.log(taskObj);
-    //taskObj.status = 'done';
-    //taskObj.priorityVal = 0;
     console.log(taskArray);
     let sortedTaskArray = sortTodoList(taskArray);
     let newTaskArray = JSON.stringify(sortedTaskArray);
